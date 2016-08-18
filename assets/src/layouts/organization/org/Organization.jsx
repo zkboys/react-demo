adbb08e1b322883e245ea634e2abd8de597db28b
wangshubin <wangshubin@xiaoyage.com>
2016-08-18 12:08:02 +0800

    页面离开事件

--- assets/src/layouts/organization/org/Organization.jsx
+++ assets/src/layouts/organization/org/Organization.jsx
@@ -10,11 +10,26 @@ export class LayoutComponent extends Component {
 
     static propTypes = {};
 
+    static contextTypes = {
+        router: React.PropTypes.object,
+    };
+
     componentWillMount() {
         const {actions} = this.props;
         actions.setPageHeaderStatus({
             hidden: true,
             title: '组织架构-自定义title',
+        });
+    }
+
+    componentDidMount() {
+        const {route} = this.props;
+        const {router} = this.context; // If contextTypes is not defined, then context will be an empty object.
+
+        router.setRouteLeaveHook(route, (/* nextLocation */) => {
+            // 返回 false 会继续停留当前页面，
+            // 否则，返回一个字符串，会显示给用户，让其自己决定
+            return '您有未保存的内容，确认要离开？';
         });
     }
 
