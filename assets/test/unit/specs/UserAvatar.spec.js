e3210558955e3b4cceddf37b02ef3ef1e82dc4b1
wangshubin <wangshubin@xiaoyage.com>
2016-08-19 12:06:00 +0800

    写测试？

--- assets/test/unit/specs/UserAvatar.spec.js
+++ assets/test/unit/specs/UserAvatar.spec.js
@@ -4,10 +4,44 @@ import UserAvatar from '../../../src/components/UserAvatar.jsx'
 
 // http://reactjs.cn/react/docs/test-utils.html
 describe('UserAvatar', () => {
-    it('往页面插入一段带有demo类的div', () => {
+
+    it('用户不存在', () => {
         let instance = ReactTestUtils.renderIntoDocument(
             <UserAvatar/>
         );
         assert.ok(ReactTestUtils.scryRenderedDOMComponentsWithTag(instance, 'span'));
+    });
+
+    it('额外class', () => {
+        const user = {
+            name: '用户名',
+        };
+        const className = 'test';
+        let instance = ReactTestUtils.renderIntoDocument(
+            <UserAvatar user={user} className={className}/>
+        );
+        assert.ok(ReactTestUtils.findRenderedDOMComponentWithClass(instance, 'test'));
+    });
+
+    it('用户存在，只有登录名', () => {
+        const user = {
+            loginname: '登录名',
+        };
+        const className = 'test';
+        let instance = ReactTestUtils.renderIntoDocument(
+            <UserAvatar user={user} className={className}/>
+        );
+        assert.ok(ReactTestUtils.scryRenderedDOMComponentsWithTag(instance, 'span'));
+    });
+
+    it('用户存在，有头像', () => {
+        const user = {
+            avatar: 'http://img0.bdstatic.com/img/image/shouye/xiaoxiao/%E5%AE%A0%E7%89%A983.jpg',
+        };
+        const className = 'test';
+        let instance = ReactTestUtils.renderIntoDocument(
+            <UserAvatar user={user} className={className}/>
+        );
+        assert.ok(ReactTestUtils.scryRenderedDOMComponentsWithTag(instance, 'img'));
     });
 });
